4af66862a195a16626c8049425418c6374b5a12b
Daniel Maestro Gomez <dmaestro@c2r11s1.42madrid.com>
2026-02-24 20:47:45 +0100

    puta vida

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -3,20 +3,20 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dmaestro <dmaestro@student.42madrid.con    +#+  +:+       +#+        */
+/*   By: dmaestro <dmaestro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/16 17:17:15 by dmaestro          #+#    #+#             */
-/*   Updated: 2026/01/16 17:58:13 by dmaestro         ###   ########.fr       */
+/*   Updated: 2026/02/24 20:23:18 by dmaestro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "includes/sed.h"
-static void replace_function(int fd_read , int fd_write, char **strs);
-static int ft_strcmp(char *str1,const char *str2);
+static void replace_function(std::ifstream& fd_read , std::ifstream& fd_write, char **strs);
+static int ft_strcmp(std::string& str1 ,const char *str2, int n, int actual_point);
 static int ft_strlen(char *str);
 int main(int args, char **argv)
 {
-    int fd_read;
+    
     int fd_replace;
 
     if(args != 5)
@@ -30,28 +30,29 @@ int main(int args, char **argv)
         std::cerr << "invalid fd for the read please put a valid fd" << std::endl;
         return(-1);
     }
-    if(ft_strcmp(argv[2] + ft_strlen(argv[2]) - 8, ".replace") != 0)
+    if(std::strncmp(argv[2] + ft_strlen(argv[2]) -8 , ".replace",  8) != 0)
     {
         std::cerr << "invalid file type for  replacement please put a valid fd" << std::endl;
         return(close(fd_read), -1);
     }
-    fd_replace = open(argv[2],  O_WRONLY | O_CREAT | O_TRUNC, 0777);
+    std::ifstream fd_read(argv[2]);
     replace_function(fd_read, fd_replace, argv + 3);
     close(fd_read);
     close(fd_replace);
     
 }
 
-static int ft_strcmp(char *str1, const char *str2)
+static int ft_strcmp(std::string& str1, const char *str2, int n, int actual_point)
 {
     int i;
     
     i = 0;
-    while(str1[i] && str2[i])
+    while(str1[actual_point] && str2[i] && i < n)
     {
-        if(str1[i] != str2[i])
-            return(str1[i] - str2[i]);
-        i++;   
+        if(str1[actual_point] != str2[i])
+            return(str1[actual_point] - str2[i]);
+        i++;
+        actual_point++;   
     }
     return(0);
 }
@@ -64,9 +65,9 @@ static int ft_strlen(char *str)
         i++;
     return(i);
 }
-static void replace_function(int fd_read , int fd_write, char **strs)
+static void replace_function(std::ifstream& fd_read , std::ifstream& fd_write, char **strs)
 {
-    char buff[100];
+    std::string buff;
     int i;
     int str1_len;
     int str2_len;
@@ -75,23 +76,27 @@ static void replace_function(int fd_read , int fd_write, char **strs)
     str2_len = ft_strlen(strs[1]);
     str1_len = ft_strlen(strs[0]);
     i = 0;
-    bytes_read = read(fd_read, buff, 100);
+      std::cout << buff << std::endl;
+    buff = std::get
+  
     while(bytes_read > 0)
     {
-        while(i < bytes_read)
+        for(size_t z = 0; z < buff.size(); z++)
         {
-            if(ft_strcmp(buff + i, strs[0]) == 0)
+            if(ft_strcmp(buff, strs[0], ft_strlen(strs[0]), z) == 0)
             {
-                i+= str1_len;
-                write(fd_write, strs[1], str2_len);
+                z += ft_strlen(strs[0]);
+                write(fd_write, strs[1],ft_strlen(strs[1]));   
             }
             else
-            { 
-                write(fd_write, &buff[i], 1);
-                i++;
-            }
+                {
+                    write(fd_write, &buff[z], 1);
+                    z++;
+                }
+            
         }
-        i = 0;
-        bytes_read = read(fd_read, buff, 100);
+        bytes_read = read(fd_read, &buff, 100);
     }
+    close(fd_write);
+    close(fd_read);
 }
